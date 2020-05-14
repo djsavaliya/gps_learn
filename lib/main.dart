93ccd952769378952b0f3a6f863d8feab6f48234
Darshil Savaliya <42993834+djsavaliya@users.noreply.github.com>
2020-05-15 03:58:22 +0530

    Showing speed in two ways,Refresh Button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,51 +10,5 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  Position _position;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Location"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if(_position != null)
-              Text("(${_position.latitude}\t,\t${_position.longitude})"),
-            FlatButton(
-              child: Text("Get location"),
-              onPressed: () {
-                _getLocation();
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _getLocation(){
-    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-
-    geolocator
-      .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-      .then((Position position){
-        setState(() {
-          _position = position;
-        });
-      });
   }
 }
